db3aa22b220eab452810926ced38214145a1d0b0
Phuong Tai <82455362+phuongtai2003@users.noreply.github.com>
2026-09-20 02:04:06 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        if(prices.isEmpty()) return 0
+
+        var left = 0
+        var right = 1
+        var maxProfit = 0
+        while(right < prices.size) {
+            if(prices[right] > prices[left]) {
+                val profit = prices[right] - prices[left]
+                maxProfit = Math.max(maxProfit, profit)
+            } else {
+                left = right
+            }
+            right++
+        }
+
+        return maxProfit
+    }
+}
